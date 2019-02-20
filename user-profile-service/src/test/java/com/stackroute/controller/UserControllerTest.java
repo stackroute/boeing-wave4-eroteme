9e9135893a9fa88887cd965ed5fd0902812b5dca
Zakir Hussain <s.zakirhussain02@gmail.com>
2019-02-20 14:08:45 +0530

    modified pom.xml

--- user-profile-service/src/test/java/com/stackroute/controller/UserControllerTest.java
+++ user-profile-service/src/test/java/com/stackroute/controller/UserControllerTest.java
@@ -28,7 +28,7 @@ import java.util.List;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.when;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
-
+//controller class
 @RunWith(SpringRunner.class)
 @WebMvcTest
 public class UserControllerTest {
