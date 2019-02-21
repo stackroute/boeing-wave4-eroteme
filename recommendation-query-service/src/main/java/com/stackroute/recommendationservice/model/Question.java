4f9c406c36d2a2f9ef859ac1279560f7b1827273
amit-s-k <amitsk2014@gmail.com>
2019-02-21 23:22:58 +0530

    Added testcases and refactored

--- recommendation-query-service/src/main/java/com/stackroute/recommendationservice/model/Question.java
+++ recommendation-query-service/src/main/java/com/stackroute/recommendationservice/model/Question.java
@@ -1,11 +1,13 @@
 package com.stackroute.recommendationservice.model;
 
+import lombok.Builder;
 import lombok.Data;
 import org.neo4j.ogm.annotation.Id;
 import org.neo4j.ogm.annotation.NodeEntity;
 
 @NodeEntity
 @Data
+@Builder
 public class Question {
     @Id
     long questionId;
