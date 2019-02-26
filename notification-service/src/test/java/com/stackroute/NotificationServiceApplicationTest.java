9ffee8364b7f852596832015f7d5fda0cbeb6f48
uday_Uthukota <uday7u7@gmail.com>
2019-02-26 09:32:05 +0530

    modified notification service

---
+++ notification-service/src/test/java/com/stackroute/NotificationServiceApplicationTest.java
@@ -0,0 +1,13 @@
+package com.stackroute;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class NotificationServiceApplicationTest {
+
+    @Test
+    public void main() {
+    }
+
+}
