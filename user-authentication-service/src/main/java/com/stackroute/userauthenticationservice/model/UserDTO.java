4b9da069c58849a0cfbcb17a350dcbc9339841c7
Mounika <julurimounika96@gmail.com>
2019-03-04 10:40:50 +0530

    added rabbitmq code user profile and authentication service

---
+++ user-authentication-service/src/main/java/com/stackroute/userauthenticationservice/model/UserDTO.java
@@ -0,0 +1,26 @@
+package com.stackroute.userauthenticationservice.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.springframework.data.annotation.Id;
+import java.util.List;
+
+    @Data
+    @NoArgsConstructor
+    @AllArgsConstructor
+    @Builder
+    public class UserDTO {
+        /*
+    assigning the primary keys
+     */
+        private String email;
+        private String password;
+        private String firstName;
+        private String lastName;
+        private List<String> interests;
+
+    }
+
+
