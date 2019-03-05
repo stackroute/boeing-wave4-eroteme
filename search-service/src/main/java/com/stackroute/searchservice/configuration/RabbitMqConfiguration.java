51af7c77c9c7174d4b78ce9faf784d88108dd2bf
amit-s-k <amitsk2014@gmail.com>
2019-03-05 15:51:07 +0530

    updated files

--- search-service/src/main/java/com/stackroute/searchservice/configuration/RabbitMqConfiguration.java
+++ search-service/src/main/java/com/stackroute/searchservice/configuration/RabbitMqConfiguration.java
@@ -5,7 +5,6 @@ import org.springframework.amqp.core.*;
 import org.springframework.amqp.rabbit.connection.ConnectionFactory;
 import org.springframework.amqp.rabbit.core.RabbitTemplate;
 import org.springframework.amqp.support.converter.Jackson2JsonMessageConverter;
-import org.springframework.amqp.support.converter.MessageConverter;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -49,7 +48,7 @@ public class RabbitMqConfiguration{
         return BindingBuilder.bind(queue1).to(exchange1).with(routingKey1);
     }
     @Bean
-    public MessageConverter jsonMessageConverter() {
+    public Jackson2JsonMessageConverter jsonMessageConverter() {
         return new Jackson2JsonMessageConverter();
     }
     @Bean
