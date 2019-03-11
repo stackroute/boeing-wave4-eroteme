ae6e333f39c896606bd15db4d4ed8bb75fcd7999
Neethu <neethubabu.babu0@gmail.com>
2019-03-11 12:44:45 +0530

    added another test case for controller

--- question-answer-service/src/test/java/com/stackroute/controller/QuestionControllerTest.java
+++ question-answer-service/src/test/java/com/stackroute/controller/QuestionControllerTest.java
@@ -26,8 +26,7 @@ import java.util.ArrayList;
 import java.util.List;
 
 import static org.junit.Assert.*;
-import static org.mockito.ArgumentMatchers.any;
-import static org.mockito.ArgumentMatchers.anyInt;
+import static org.mockito.ArgumentMatchers.*;
 import static org.mockito.Mockito.when;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 
@@ -95,6 +94,31 @@ public class QuestionControllerTest {
                 .andExpect(MockMvcResultMatchers.status().isNotFound())
                 .andDo(MockMvcResultHandlers.print());
     }
+
+    @Test
+    public void addCommentSuccess() throws Exception{
+        when(questionService.addQuestionComment(77,comment)).thenReturn(question);
+
+        when(questionService.addQuestionComment(anyInt(),any())).thenReturn(question);
+        mockMvc.perform(MockMvcRequestBuilders.put("/api/v1/question/comment/77")
+                .contentType(MediaType.APPLICATION_JSON).content(asJsonString(question)))
+                .andExpect(MockMvcResultMatchers.status().isOk())
+                .andDo(MockMvcResultHandlers.print());
+    }
+
+    @Test
+    public void addCommentReplySuccess() throws Exception{
+        List<Replies> repliesList = new ArrayList<>();
+        repliesList.add(reply);
+        when(questionService.addQuestionCommentReply(77,"abcd",repliesList)).thenReturn(question);
+
+        when(questionService.addQuestionCommentReply(anyInt(),anyString(),any())).thenReturn(question);
+        mockMvc.perform(MockMvcRequestBuilders.put("/api/v1/question/comment/reply/77")
+                .contentType(MediaType.APPLICATION_JSON).content(asJsonString(question)))
+                .andExpect(MockMvcResultMatchers.status().isOk())
+                .andDo(MockMvcResultHandlers.print());
+    }
+
 
     private static String asJsonString(final Object obj) {
         try {
