697d9b04a370f1cd0e8ac8461df2ff7c18642e31
sunidhi.prasad <sunidhi.prasad@boeing.com>
2019-03-15 13:12:11 +0530

    removed edit-profile

--- edit-profile/README.md
+++
@@ -1,5 +0,0 @@
-   # edit-profile service
-
-    This service provides backend support for editing a user profile.
-    
-    
--- edit-profile/src/main/java/com/stackroute/editprofile/repository/EditProfileRepository.java
+++
@@ -1,7 +0,0 @@
-package com.stackroute.editprofile.repository;
-
-import com.stackroute.editprofile.domain.UserCurrent;
-import org.springframework.data.mongodb.repository.MongoRepository;
-
-public interface EditProfileRepository extends MongoRepository<UserCurrent, String> {
-}
