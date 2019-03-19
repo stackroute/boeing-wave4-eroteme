7b00a0d968d094eef67da643927341c964b1df08
Mounika <julurimounika96@gmail.com>
2019-03-19 18:19:22 +0530

    modified notification service

--- notification-service/src/main/java/com/stackroute/service/Notifications.java
+++ notification-service/src/main/java/com/stackroute/service/Notifications.java
@@ -1,35 +1,46 @@
 package com.stackroute.service;
 
+import com.stackroute.repository.NotificationRepository;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.messaging.simp.SimpMessagingTemplate;
 import org.springframework.stereotype.Component;
+import org.springframework.stereotype.Service;
 
+import java.util.ArrayList;
 import java.util.List;
 
 @Component
-//methods for various types of notifications
+@Service
+@Slf4j
 public class Notifications {
 
     @Autowired
     private SimpMessagingTemplate template;
 
-    //notifies all the users sent by recommendation service regarding question which has been posted
-    public void generateQuestionNotification(List< String> user,String Question) {
-        for (int i = 0; i < user.size(); i++) {
-            template.convertAndSend("/queue/" + user.get(i), "Can you answer this:" + Question);
+    @Autowired
+    private NotificationRepository notificationRepository;
+
+    com.stackroute.domain.Notifications notifications=new com.stackroute.domain.Notifications();
+    List<String> notify=new ArrayList<String>();
+    String note="";
+
+    public void generateNotifications(String email){
+        //extract notification from repo for this email
+        try{
+        notifications=notificationRepository.findById(email);
+        notify=notifications.getNotification();
+        for(int i=0;i<notify.size();i++) {
+            note=notify.get(i);
+            template.convertAndSend("/queue/" +notifications.getEmail(),note);
+            log.info("notified "+email);
+        }
+        notificationRepository.delete(email);
+        log.info("deleted "+email);
+        }
+        catch(Exception e){
+            System.out.println("exception in generate notifications ");
         }
     }
-
-    //notifies the user that your question has been answered
-    public void generateAnswerNotification(String user,String Question) {
-        template.convertAndSend("/queue/" + user, "Your question:" + Question + "has been answered!");
-    }
-
-    //notifies the user that thier answer has been accepted
-    public void generateLikedNotification(String user,String Question) {
-        template.convertAndSend("/queue/" + user, "Your Answer to the  question:" + Question + "has been accepted!");
-    }
-
-
 
 }
